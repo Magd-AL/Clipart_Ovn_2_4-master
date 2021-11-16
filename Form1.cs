faa2ba5fca00c91fcc7a10dc1aa12b0da7e92f65
Magd-AL <72021118+Magd-AL@users.noreply.github.com>
2021-11-16 10:22:15 +0100

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clipart_Ovn_2_4
+{
+    public partial class frmClipart : Form
+    {
+        public frmClipart()
+        {
+            InitializeComponent();
+        }
+
+        private void frmClipart_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ clipart_ö24.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clipart_Ovn_2_4
+{
+    class Figur
+    {
+        public float bredd;
+        public float höjd;
+
+        public Figur(float bredd, float höjd)
+        {
+            this.bredd = bredd;
+            this.höjd = höjd;
+        }
+    }
+
+    class Cirkel : Figur
+    {
+        public Cirkel(float bredd, float höjd) : base(bredd, höjd)
+        {
+
+        }
+
+        public float BeräknaArena()
+        {
+            return bredd * (float)Math.PI;
+        }
+    }
+
+    class Linjel : Figur
+    {
+        public Linjel(float bredd, float höjd) : base(bredd, höjd)
+        {
+
+        }
+    }
+}
